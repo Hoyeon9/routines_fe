b17b83cf8c196c1ac95289633259fc4c7da66c4c
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-01 23:02:49 +0900

    Add delete routine operations

--- src/Components/routine/Routine.js
+++ src/Components/routine/Routine.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Routine({name, desc}){
+export default function Routine({name, desc, id, setReload}){
+    const [openConfirm, setOpenConfirm] = useState(false);
     return (
-        <div className="routine">
+        <div className="routine" style={{border: "1px solid yellow"}}>
             <div className="routine-name">{name}</div>
             <div className="routine-desc">{desc}</div>
+            {openConfirm ? <Confirm id={id} setOpenConfirm={setOpenConfirm} setReload={setReload}/> : <button onClick={() => {setOpenConfirm(true)}}>삭제</button>}
+        </div>
+    )
+}
+
+function Confirm({id, setOpenConfirm, setReload}){
+    const handleDelete = () => {
+        const userId = sessionStorage.getItem('userId');
+        const serverAddress = process.env.REACT_APP_SERVER_ADDRESS;
+        fetch(`${serverAddress}/api/routine`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({userId, 'routineId': id})
+        })
+        .then(res => {
+            if(!res.ok){
+                throw new Error(`HTTP error: ${res.status}`)
+            }
+            alert('루틴이 삭제되었습니다.')
+            setOpenConfirm(false);
+            setReload(true);
+        })
+    }
+    return(
+        <div className="confirm-modal">
+            정말 삭제하시겠습니까?
+            <button onClick={handleDelete}>확인</button>
+            <button onClick={() => {setOpenConfirm(false);}}>취소</button>
         </div>
     )
 }
--- src/Components/routine/RoutineList.js
+++ src/Components/routine/RoutineList.js
@@ -5,6 +5,7 @@ export default function RoutineList(){
     const serverAddress = process.env.REACT_APP_SERVER_ADDRESS;
     const userId = sessionStorage.getItem('userId')
     const [routines, setRoutines] = useState([]);
+    const [reload, setReload] = useState(false);
     useEffect(()=>{
         fetch(`${serverAddress}/api/routine/all?userId=${userId}`, {
             method: 'GET',
@@ -24,11 +25,12 @@ export default function RoutineList(){
         .catch(err => {
             console.error(err);
         })
-    }, [])
+        setReload(false);
+    }, [reload])
     return (
         <div className="routine-list-container">
             {userId ? <div className="routine-list">
-                {routines.map((routine, idx) => <Routine key={idx} name={routine.routineName} desc={routine.routineDesc} />)}
+                {routines.map((routine, idx) => <Routine key={idx} name={routine.routineName} desc={routine.routineDesc} id={routine.routineId} setReload={setReload}/>)}
             </div>
              : <div className="error-page">404</div>}
         </div>
