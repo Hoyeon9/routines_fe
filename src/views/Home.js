46c5cc3b6e85e195267a0bd0f0ec10ff20a6f05f
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-01 22:41:30 +0900

    Make logout button

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home(){
     const navigate = useNavigate();
-    const userId = sessionStorage.getItem('userId');
+    const [userId, setUserId] = useState(sessionStorage.getItem('userId'))
     return(
         <div>
             {userId ?
+                <>
                 <button onClick={() => {navigate("/routines")}}>내 루틴</button>
+                <button onClick={() => {sessionStorage.removeItem('userId'); setUserId(null);}}>로그아웃</button>
+                </>
                 : <button onClick={() => {navigate("/login")}}>로그인</button>
             }
         </div>
